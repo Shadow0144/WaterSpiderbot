765093a64facdd1b1767c525889471497df7f178
Shadow0144 <corbincogswell@gmail.com>
2026-07-08 08:06:33 -0500

    [feat] Adds a basic spider model with eight legs, a head, and two eyes (cameras), and adjusts the camera so it is pointed the right way to easily view the simulation

--- spider_test.py
+++ spider_test.py
@@ -2,10 +2,17 @@ import time
 import mujoco
 import mujoco.viewer
 
-model = mujoco.MjModel.from_xml_path('test.xml')
+model = mujoco.MjModel.from_xml_path('spider_test.xml')
 data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
+    viewer.cam.azimuth = 270
+    viewer.cam.elevation = -20
+    viewer.cam.distance = 2.0
+    viewer.cam.lookat[:] = [0, 0, 0.25]
+
+    viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CAMERA] = 1
+
     while viewer.is_running():
         step_start = time.time()
         mujoco.mj_step(model, data)
