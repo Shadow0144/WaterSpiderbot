f56cf5d3a66da98e642ca98d00d9d3d89ebfc513
Shadow0144 <corbincogswell@gmail.com>
2026-07-07 19:44:35 -0500

    [feat] Adds a simple scene with a ball and a simple script to test it with a viewer

---
+++ sim.py
@@ -0,0 +1,16 @@
+import time
+import mujoco
+import mujoco.viewer
+
+model = mujoco.MjModel.from_xml_path('test.xml')
+data = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        step_start = time.time()
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        time_until_next_step = model.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
+
