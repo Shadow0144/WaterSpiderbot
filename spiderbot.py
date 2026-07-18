20b3aadd5d61a3a61152745eb2bf71fd1c4bbdf2
Shadow0144 <corbincogswell@gmail.com>
2026-07-18 14:01:03 -0500

    [chore] Renames the spiderbot parts to be more anatomically correct (and shorter)

--- spiderbot.py
+++ spiderbot.py
@@ -12,26 +12,26 @@ class spiderleg:
 
     def load_leg(self):
         try:
-            self.servo_hip_actuator_id = self.model.actuator("servo_" + self.id + "_hip_pitch").id
-            self.servo_upper_leg_actuator_id = self.model.actuator("servo_" + self.id + "_upper_leg_pitch").id
-            self.servo_lower_leg_actuator_id = self.model.actuator("servo_" + self.id + "_lower_leg_pitch").id
+            self.servo_coxa_actuator_id = self.model.actuator("servo_" + self.id + "_coxa_pitch").id
+            self.servo_femur_actuator_id = self.model.actuator("servo_" + self.id + "_femur_pitch").id
+            self.servo_tibia_actuator_id = self.model.actuator("servo_" + self.id + "_tibia_pitch").id
         except KeyError:
-            print(f"Key error")
+            print(f"Key error: " + self.id)
             exit()
 
-    def set_hip_target(self, target_angle_rad):
-        self.data.ctrl[self.servo_hip_actuator_id] = target_angle_rad
+    def set_coxa_target(self, target_angle_rad):
+        self.data.ctrl[self.servo_coxa_actuator_id] = target_angle_rad
 
-    def set_upper_leg_target(self, target_angle_rad):
-        self.data.ctrl[self.servo_upper_leg_actuator_id] = target_angle_rad
+    def set_femur_target(self, target_angle_rad):
+        self.data.ctrl[self.servo_femur_actuator_id] = target_angle_rad
 
-    def set_lower_leg_target(self, target_angle_rad):
-        self.data.ctrl[self.servo_lower_leg_actuator_id] = target_angle_rad
+    def set_tibia_target(self, target_angle_rad):
+        self.data.ctrl[self.servo_tibia_actuator_id] = target_angle_rad
 
-    def set_leg_targets(self, hip_target_angle_rad, upper_leg_target_angle_rad, lower_leg_target_angle_rad):
-        self.set_hip_target(hip_target_angle_rad)
-        self.set_upper_leg_target(upper_leg_target_angle_rad)
-        self.set_lower_leg_target(lower_leg_target_angle_rad)
+    def set_leg_targets(self, coxa_target_angle_rad, femur_target_angle_rad, tibia_target_angle_rad):
+        self.set_coxa_target(coxa_target_angle_rad)
+        self.set_femur_target(femur_target_angle_rad)
+        self.set_tibia_target(tibia_target_angle_rad)
 
 
 class spiderbot:
@@ -42,15 +42,15 @@ class spiderbot:
 
     def load_model(self):
         # Left side
-        self.left_back_leg = spiderleg(self.model, self.data, "left_back")
-        self.left_center_back_leg = spiderleg(self.model, self.data, "left_center_back")
-        self.left_center_front_leg = spiderleg(self.model, self.data, "left_center_front")
-        self.left_front_leg = spiderleg(self.model, self.data, "left_front")
+        self.left_i_leg = spiderleg(self.model, self.data, "left_i")
+        self.left_ii_leg = spiderleg(self.model, self.data, "left_ii")
+        self.left_iii_leg = spiderleg(self.model, self.data, "left_iii")
+        self.left_iv_leg = spiderleg(self.model, self.data, "left_iv")
         # Right side
-        self.right_back_leg = spiderleg(self.model, self.data, "right_back")
-        self.right_center_back_leg = spiderleg(self.model, self.data, "right_center_back")
-        self.right_center_front_leg = spiderleg(self.model, self.data, "right_center_front")
-        self.right_front_leg = spiderleg(self.model, self.data, "right_front")
+        self.right_i_leg = spiderleg(self.model, self.data, "right_i")
+        self.right_ii_leg = spiderleg(self.model, self.data, "right_ii")
+        self.right_iii_leg = spiderleg(self.model, self.data, "right_iii")
+        self.right_iv_leg = spiderleg(self.model, self.data, "right_iv")
 
     def walk_forward(self, time):
         sin_phase = np.sin(time)
@@ -59,18 +59,18 @@ class spiderbot:
         sin_half_phase = np.sin(time + (2.0*math.pi/3.0))
         cos_half_phase = np.cos(time + (2.0*math.pi/3.0))
 
-        hip_target_angle = math.radians(30)
-        upper_leg_target_angle = math.radians(45)
-        lower_leg_target_angle = math.radians(5)
+        coxa_target_angle = math.radians(30)
+        femur_target_angle = math.radians(45)
+        tibia_target_angle = math.radians(5)
 
         # Left side
-        self.left_back_leg.set_leg_targets(-hip_target_angle * sin_phase, upper_leg_target_angle * sin_half_phase, lower_leg_target_angle * sin_half_phase)
-        self.left_center_back_leg.set_leg_targets(-hip_target_angle * cos_phase, upper_leg_target_angle * cos_half_phase, lower_leg_target_angle * cos_half_phase)
-        self.left_center_front_leg.set_leg_targets(-hip_target_angle * sin_phase, upper_leg_target_angle * sin_half_phase, lower_leg_target_angle * sin_half_phase)
-        self.left_front_leg.set_leg_targets(-hip_target_angle * cos_phase, upper_leg_target_angle * cos_half_phase, lower_leg_target_angle * cos_half_phase)
+        self.left_i_leg.set_leg_targets(-coxa_target_angle * cos_phase, femur_target_angle * cos_half_phase, tibia_target_angle * cos_half_phase)
+        self.left_ii_leg.set_leg_targets(-coxa_target_angle * sin_phase, femur_target_angle * sin_half_phase, tibia_target_angle * sin_half_phase)
+        self.left_iii_leg.set_leg_targets(-coxa_target_angle * cos_phase, femur_target_angle * cos_half_phase, tibia_target_angle * cos_half_phase)
+        self.left_iv_leg.set_leg_targets(-coxa_target_angle * sin_phase, femur_target_angle * sin_half_phase, tibia_target_angle * sin_half_phase)
         
         # Right back
-        self.right_back_leg.set_leg_targets(hip_target_angle * cos_phase, upper_leg_target_angle * cos_half_phase, lower_leg_target_angle * cos_half_phase)
-        self.right_center_back_leg.set_leg_targets(hip_target_angle * sin_phase, upper_leg_target_angle * sin_half_phase, lower_leg_target_angle * sin_half_phase)
-        self.right_center_front_leg.set_leg_targets(hip_target_angle * cos_phase, upper_leg_target_angle * cos_half_phase,  lower_leg_target_angle * cos_half_phase)
-        self.right_front_leg.set_leg_targets(hip_target_angle * sin_phase, upper_leg_target_angle * sin_half_phase, lower_leg_target_angle * sin_half_phase)
+        self.right_i_leg.set_leg_targets(coxa_target_angle * sin_phase, femur_target_angle * sin_half_phase, tibia_target_angle * sin_half_phase)
+        self.right_ii_leg.set_leg_targets(coxa_target_angle * cos_phase, femur_target_angle * cos_half_phase,  tibia_target_angle * cos_half_phase)
+        self.right_iii_leg.set_leg_targets(coxa_target_angle * sin_phase, femur_target_angle * sin_half_phase, tibia_target_angle * sin_half_phase)
+        self.right_iv_leg.set_leg_targets(coxa_target_angle * cos_phase, femur_target_angle * cos_half_phase, tibia_target_angle * cos_half_phase)
