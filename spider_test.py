4c59be171aea09b0463e7f84834c78824cf061ce
Shadow0144 <corbincogswell@gmail.com>
2026-07-19 18:27:48 -0500

    [feat] Changes the spiderbot to wait 5 seconds before beginning the walk forward animation

--- spider_test.py
+++ spider_test.py
@@ -15,11 +15,18 @@ def main():
 
         viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CAMERA] = 1
 
+        time_until_start_sec = 5.0 # 5 seconds
+        last_step = time.time()
+
         while viewer.is_running():
             step_start = time.time()
             mujoco.mj_step(spider.model, spider.data)
 
-            spider.walk_forward(step_start)
+            if time_until_start_sec <= 0.0:
+                spider.walk_forward(step_start)
+            else:
+                time_until_start_sec -= (step_start - last_step)
+                last_step = step_start
 
             viewer.sync()
             time_until_next_step = spider.model.opt.timestep - (time.time() - step_start)
