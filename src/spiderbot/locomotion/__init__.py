c2aadfbdd1a685639018dc5d3a70c1a33324f2c0
Shadow0144 <corbincogswell@gmail.com>
2026-07-21 16:48:45 -0500

    [feat] Adds a new locomotion module setup with two new locomotion modules, fixes the ranges for the tibia hinge, and enables visualizing the joints

---
+++ src/spiderbot/locomotion/__init__.py
@@ -0,0 +1,7 @@
+from .simple_sin import SimpleSinLocomotionModule
+from .handcrafted import HandcraftedLocomotionModule
+
+__all__ = [
+    "SimpleSinLocomotionModule",
+    "HandcraftedLocomotionModule",
+]
---
+++ src/spiderbot/locomotion/locomotion.py
@@ -0,0 +1,3 @@
+class LocomotionModule:
+    def walk_forward(self, time, leg_set):
+        pass
